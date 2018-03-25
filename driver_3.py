461f65e7eacf0aec80ff97efeca0ef72a100275e
Vladimir Popovic <valdinijan@gmail.com>
2018-03-25 23:18:14 +0200

    continued Solver and Board

--- driver_3.py
+++ driver_3.py
@@ -20,34 +20,60 @@ class Move:
 class Fringe:
     def __init__(self):
         pass
-    def add(self, item):
+    def push(self, item):
         pass
-    def remove(self):
+    def pop(self):
         pass
 
 class Queue(Fringe):
     def __init__(self):
         self.items = []
-    def add(self, item):
+    def push(self, item):
         self.items.insert(0,item)
-    def remove(self):
+    def pop(self):
         return self.items.pop()
     def isEmpty(self):
         return self.items == []
+
+class Stack(Fringe):
+    pass
+    
+class PrioQueue(Fringe):
+    pass
 
 class Board:
     GOAL_STATE = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     def __init__(self, state):
         # state: list of integers
         self.state = state
-    def goalTest(state):
-        if len(state) != 9:
+    def goalTest(self):
+        if len(self.state) != 9:
             return False
-        for idx in state:
-            if state[idx] != Board.GOAL_STATE[idx]:
+        for idx in range(len(self.state)):
+            if self.state[idx] != Board.GOAL_STATE[idx]:
                 return False
         return True
-
+    def move(self, dir):
+        idxZ = self.state.index(0)
+        if dir == Move.UP:
+            if idxZ >= 3:
+                idxNew = idxZ - 3
+                tmp = self.state[idxNew]
+                self.state[idxNew] = 0
+                self.state[idxZ] = tmp
+                return True
+            else
+                return False
+        if dir == Move.DOWN:
+            if idxZ >= 3:
+                idxNew = idxZ - 3
+                tmp = self.state[idxNew]
+                self.state[idxNew] = 0
+                self.state[idxZ] = tmp
+                return True
+            else
+                return False
+        
 class Node:
     def __init__(self, board):
         self.board = board
@@ -56,23 +82,47 @@ class Node:
         self.parent = None
 
 class State:
-    def __init__(self):
+    def __init__(self, algType, startNode):
         # fringe: list of Board objects
-        self.fringe = Fringe()
+        if algType == 'bfs':
+            self.fringe = Queue()
+        elif algType == 'dfs':
+            self.fringe = Stack()
+        elif algType == 'ast':
+            self.fringe = PrioQueue()
+
+        self.fringe.push(startNode)
+        
         # path: list of Move objects
         self.path = []
         # visited: set of Node objects
-        self.explored = {}
+        self.explored = set()
 
-class Solver:
+class Solver(State):
     class SolverOutput:
         pass
 
-    def __init__(self, algType, start):
-        self.algType = algType
-        self.start = start
-    
+    def __init__(self, algType, startNode):
+        super().__init__(algType, startNode)
+        
     def run(self):
+        result = False
+        cnt = 0
+        while not self.fringe.isEmpty() and result == False and cnt < 10:
+            print("In a loop")
+            cnt = cnt + 1
+            currNode = self.fringe.pop()
+            self.explored.add(currNode)
+            
+            # print(currNode.state)
+            
+            # check if the goal is reached
+            if currNode.goalTest():
+                result = True
+
+            # expand and add neighbors to the fringe
+            
+
         slOut = Solver.SolverOutput()
         slOut.pathToGoal = [ 'Bag', 'Wuss' ]
         slOut.costOfGoal = 0
@@ -114,13 +164,24 @@ def PrepareFile(params):
 # Experimental
 #===============================================================================
 
-    
+'''
+b = Board([1, 2, 3])
+print("b1: ", b.goalTest())
+b = Board([0, 1, 2, 3, 4, 5, 6, 7, 8])
+print("b1: ", b.goalTest())
+b = Board([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print("b2: ", b.goalTest())
+b = Board([0, 1, 2, 4, 3, 5, 6, 7, 8])
+print("b3: ", b.goalTest())
+'''
+
 #===============================================================================
 # main
 #===============================================================================
 
 # execute algorithm
-solver = Solver(gAlg, gInitState)
+initState = Board(gInitState)
+solver = Solver(gAlg, initState)
 slOut = solver.run()
 print("Solver output: ", slOut.pathToGoal)
 fileStr = PrepareFile(slOut)
